7cbe51dba1a5c966713d26eeec46bb97bc52993b
Vilvaramadurai  Samidurai <vsamidurai@groupon.com>
2021-08-26 15:58:37 +0100

    * Make kubeconfig_api_version configurable in templates kubeconfig.tpl to support client.authentication.k8s.io/v1beta

--- local.tf
+++ local.tf
@@ -177,6 +177,7 @@ locals {
     kubeconfig_name                   = local.kubeconfig_name
     endpoint                          = coalescelist(aws_eks_cluster.this[*].endpoint, [""])[0]
     cluster_auth_base64               = coalescelist(aws_eks_cluster.this[*].certificate_authority[0].data, [""])[0]
+    aws_authenticator_kubeconfig_apiversion = var.kubeconfig_api_version
     aws_authenticator_command         = var.kubeconfig_aws_authenticator_command
     aws_authenticator_command_args    = length(var.kubeconfig_aws_authenticator_command_args) > 0 ? var.kubeconfig_aws_authenticator_command_args : ["token", "-i", coalescelist(aws_eks_cluster.this[*].name, [""])[0]]
     aws_authenticator_additional_args = var.kubeconfig_aws_authenticator_additional_args
